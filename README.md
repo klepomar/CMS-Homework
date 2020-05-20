f9dbc373a6fae7e72e757ed88b8d77b1ff155407
root <root@DESKTOP-QNNTQPL.localdomain>
2020-05-20 11:45:23 +0200

    Final version

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # CMS-Homework
+
+This small program is used for plotting data from csv file.
+It is possible to choose which sample and which lines (spectrum) we want to plot
+into graph.
+
+There are also meta data for which I didnt find purpose at this demo. But they are saved and it is possible to work with them if needed.
+
+Application is run from main.py. But all operations are corporated into class plotHandler.User can interact with application using command line first time he is asked which sample wants to use. If number is bigger than number of samples store in memory then he is informed about this and can enter sample number again. After choosing sample user can define which lines need to be plotted. If all lines need to be plotted it is neccesery to write all into command line (not case sensitive).
+Interval ends are seprated by ','. It is also important to start with smaller number and end with bigger. Again if any problem occure. You are inform about invalidity of your input and you can try again. 
+
+Modules required are as follow. Matplotlib, Pandas, numPy. These are standart modules for Python. They should not cause any problem. Used version of Python is 3.8
+
+I encountered 1 bug. Sometimes dataframe plot whil throw exception. I resolved to try except syntax use. And was able to plot it anyway.
+
+
+Marco Klepoch
--- plotManipulator.py
+++ plotManipulator.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 class plotManipulator:
 
     def __init__(self, *args, **kwargs):
+        """Init function, csv file is loaded here and split into sample"""
+        """ALso metada are stored and can be use later."""
+        """Number of rows is also cut from all blank lines"""
         self.file = pd.read_csv(args[0], header=1, delimiter=",")
         rng = [2 * n for n in range(1, len(self.file.columns) // 2)]
         self.dfs = np.split(self.file, rng, axis=1)
@@ -18,13 +21,9 @@ class plotManipulator:
             pom_cela = pom.iloc[:index]
             self.meta_data.append(pom.iloc[index:])
             self.dfs[n] = pd.DataFrame(pom_cela)
-            try:
-                self.dfs[n].plot(x="Abs", y="Wavelength (nm)")
-            except:
-                self.dfs[n] = self.dfs[0].astype(float)
-                self.dfs[n].plot(x="Abs", y="Wavelength (nm)")
 
     def which_samples(self):
+        """Function for determine which sample wants to choose. Accept integer"""
         print("Input number of sample which you want to use for plot.\n")
         while True:
             self.sample = input()
@@ -36,6 +35,8 @@ class plotManipulator:
                 continue
 
     def which_lines(self):
+        """Function for determine which interval wants to choose."""
+        """ Accept integer and strin all(Not Case Sensitive). And prepare to plot"""
         print("Input interval of lines which you want to plot, separated by ',' ."
               "If you want to use all lines type all.\n")
 
@@ -56,5 +57,11 @@ class plotManipulator:
                 continue
 
     def plot_Graph(self):
-        self.to_plot.plot(x="Abs", y="Wavelength (nm)")
-        plt.show()
+        """Function for plotting"""
+        try:
+            self.to_plot.plot(x="Abs", y="Wavelength (nm)")
+            plt.show()
+        except:
+            self.to_plot = self.to_plot.astype(float)
+            self.to_plot.plot(x="Abs", y="Wavelength (nm)")
+            plt.show()
